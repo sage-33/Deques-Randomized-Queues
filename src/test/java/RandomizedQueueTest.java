df819c6f468d869300ec195cc4e3cda253b0aaa9
Sage Silberman <ssilberman2023@chadwickschool.org>
2022-06-05 22:02:19 -0700

    feat: Implement RandomizedQueueTest

--- src/test/java/RandomizedQueueTest.java
+++ src/test/java/RandomizedQueueTest.java
@@ -1,5 +1,7 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -14,32 +16,69 @@ public class RandomizedQueueTest {
 
 	@Test
 	public void testIsEmpty() {
-		fail("Not yet implemented");
+		ArrayList<String> expectedResult = new ArrayList<String>();
+		assertTrue(list.isEmpty());
+		expectedResult.add("Hello");
+		list.enqueue("Hello");
+		String actualResult = list.dequeue();
+		assertTrue(list.isEmpty());
 	}
 
 	@Test
 	public void testSize() {
-		fail("Not yet implemented");
+		ArrayList<String> expectedResult = new ArrayList<String>();
+		expectedResult.add("Hello");
+		assertEquals(1, expectedResult.size());
+		expectedResult.add("World");
+		assertEquals(2, expectedResult.size());
+		assertTrue(list.size() == 0);
+		list.enqueue("hello world");
+		assertTrue(list.size() == 1);
+		list.dequeue();
+		assertTrue(list.size() == 0);
 	}
 
 	@Test
 	public void testEnqueue() {
-		fail("Not yet implemented");
+		list.enqueue("One");
+		assertEquals(1, list.size());
+		list.enqueue("Two");
+		assertEquals(2, list.size());
+		list.enqueue("Three");
+		assertEquals(3, list.size());
+		list.enqueue("Four");
+		assertEquals(4, list.size());
 	}
 
 	@Test
 	public void testDequeue() {
-		fail("Not yet implemented");
+		ArrayList<String> expectedResult = new ArrayList<String>();
+		expectedResult.add("Hello");
+		expectedResult.add("World");
+		list.enqueue("Hello");
+		list.enqueue("World");
+		String actualResult = list.dequeue();
+		assertTrue("Should contain " + actualResult, expectedResult.contains(actualResult));
+		expectedResult.remove(actualResult);
+		actualResult = list.dequeue();
+		assertTrue("Should contain the other word " + actualResult, expectedResult.contains(actualResult));
 	}
 
 	@Test
 	public void testSample() {
-		fail("Not yet implemented");
+		ArrayList<String> expectedResult = new ArrayList<String>();
+		expectedResult.add("Hello");
+		expectedResult.add("World");
+		list.enqueue("Hello");
+		list.enqueue("World");
+		String actualResult = list.sample();
 	}
 
 	@Test
 	public void testIterator() {
-		fail("Not yet implemented");
+		ArrayList<String> expectedResult = new ArrayList<String>();
+		expectedResult.add("Hello");
+		expectedResult.add("World");
 	}
 
 }
